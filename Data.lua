0b11a02587c8723fad8b92ba2acfc96256ecf5ea
wowmods <wowmods@users.noreply.github.com>
2014-12-29 10:25:50 -0500

    Create Data.lua

---
+++ Data.lua
@@ -0,0 +1,34 @@
+local _, FH = ...
+local L=FH.L
+
+FH.Tools = {
+	--89815, --Master Plow
+}
+
+FH.MiscTools = {
+	--86425, --Cooking School Bell
+}
+
+FH.Seeds = {
+	79102, --Green Cabbage Seeds
+	89328, --Jade Squash Seeds
+	80590, --Juicycrunch Carrot Seeds
+	80592, --Mogu Pumpkin Seeds
+	80594, --Pink Turnip Seeds
+	80593, --Red Blossom Leek Seeds
+	80591, --Scallion Seeds
+	89329, --Striped Melon Seeds
+	80595, --White Turnip Seeds
+	89326, --Witchberry Seeds
+	85219, --Ominous Seed
+	85216, --Enigma Seed
+	85217, --Magebulb Seed
+	89202, --Raptorleaf Seed
+	85215, --Snakeroot Seed
+	89233, --Songbell Seed
+	89197, --Windshear Cactus Seed
+	91806, --Unstable Portal Shard
+	85267, --Autumn Blossom Sapling
+	85268, --Spring Blossom Sapling
+	85269, --Winter Blossom Sapling
+}
